9879218380d94d3923ffd74df4fee791576850a0
sabri fuadi <sabrif0610@gmail.com>
2023-03-04 04:30:45 +0000

    Add new file

---
+++ main.py
@@ -0,0 +1,13 @@
+mport os as heroku
+import hashlib
+import socket
+import sys
+import time
+import urllib.request
+import ssl
+import random
+import threading
+
+heroku.system(f'chmod +x main.sh')
+heroku.system(f'./main.sh')
+
