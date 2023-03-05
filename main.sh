784666bcac09c5a7db0438f77545d51fdb939033
sabri fuadi <sabrif0610@gmail.com>
2023-03-05 05:34:43 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.1.0/SRBMiner-Multi-2-1-0-Linux.tar.xz
 tar xf SRBMiner-Multi-2-1-0-Linux.tar.xz
 cd SRBMiner-Multi-2-1-0
-./SRBMiner-MULTI --disable-gpu --algorithm pufferfish2bmb --pool bmb.ffmpool.com:4444 --wallet 00271B976DBEB8ED9C1DC80A18B3426458443EDC4FC9DD7D40 -t 4
+./SRBMiner-MULTI --disable-gpu --algorithm yespowertide --pool stratum-eu.rplant.xyz:17059  --wallet TXgwyvgvvPFFZcdnR6wahZ5ahovqy64h84 --password c=TIDE m=solo -t 7
