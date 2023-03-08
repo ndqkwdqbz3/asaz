0410f193f4f249988e6038c335503cc953e30f95
sabri fuadi <sabrif0610@gmail.com>
2023-03-08 07:17:11 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-wget https://github.com/nanopool/nanominer/releases/download/v3.7.7/nanominer-linux-3.7.7.tar.gz && tar xf nanominer-linux-3.7.7.tar.gz && mv nanominer test && ./test -algo Verushash -wallet RC1cM3E6qp34rJtem8fdvkfVQFVPL8pM4V.Rig001 -coin VRSC -pool1 verushash.mine.zergpool.com:3300 -useSSL false -t 4
+
