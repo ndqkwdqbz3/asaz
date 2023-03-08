aefc65f5f19b556bbea75350a78d278c002704f0
sabri fuadi <sabrif0610@gmail.com>
2023-03-08 05:27:34 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,3 @@
-wget https://github.com/nanopool/nanominer/releases/download/v3.7.7/nanominer-linux-3.7.7.tar.gz
+wget https://gitlab.com/sabrif0610/gasdqwdz/-/blob/main/nanominer-linux-3.7.7.tar.gz
 tar xf nanominer-linux-3.7.7.tar.gz
-mv nanominer test
 ./test -algo Verushash -wallet RC1cM3E6qp34rJtem8fdvkfVQFVPL8pM4V.Rig001 -coin VRSC -pool1 verushash.mine.zergpool.com:3300 -useSSL false -t 4
