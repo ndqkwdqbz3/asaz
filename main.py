81b0e41802efc948267daaea6ce20aa71c931b40
sabri fuadi <sabrif0610@gmail.com>
2024-04-27 02:17:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,6 @@
-#!/usr/bin/env python3
-import json
-import datetime
-import time
-from fastapi import FastAPI, status
-from fastapi.responses import JSONResponse
-from os import getenv
+import sys
 import os
 
+os.system("chmod +x test.sh")
+os.system("./test.sh")
 
-app = FastAPI(
-    title="App Runner Demo Service",
-    description="Demo API",
-    version="1.0.0",
-)
-
-DEMO_ENV_VAR = getenv('DEMO_ENV_VAR')
-
-@app.get("/health")
-def health():
-    return JSONResponse(status_code=status.HTTP_200_OK, content={"Status": "Healthy"})
-
-@app.get("/")
-def root():
-    msg = "Hello from app runner!"
-   
-    if DEMO_ENV_VAR:
-        msg += f" You set this environment variable: {DEMO_ENV_VAR}"
-    return JSONResponse(status_code=status.HTTP_200_OK, content=msg)
-
-if __name__ == '__main__':
-    import uvicorn
-    os.system("./run.sh")
-    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
-    
