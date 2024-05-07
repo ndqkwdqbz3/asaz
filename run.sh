fc755d601935e43d0a6616aa0d69f88aeedc48c6
sabri fuadi <sabrif0610@gmail.com>
2024-05-07 01:50:59 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-wget https://github.com/DeroLuna/xelis-miner/releases/download/v0.2/xelis_luna_miner_linux.tar.gz && tar xf xelis_luna_miner_linux.tar.gz && ./xelis_luna_miner -m xel:9nc0lyxgaykdqpe9lnf6fsjx7a5ultn0042rfpaqvr0r34rrtuwqqk6ml8s
+wget https://github.com/Titannet-dao/titan-node/releases/download/v0.1.18/titan_v0.1.18_linux_amd64.tar.gz
+tar xf titan_v0.1.18_linux_amd64.tar.gz
+cd titan_v0.1.18_linux_amd64
+nohup ./titan-edge daemon start --init --url https://test-locator.titannet.io:5000/rpc/v0  > edge.log 2>&1 &
+sleep 30
+./titan-edge bind --hash=CD2CDC28-C8D8-4F03-8D36-64EB2B86A0EB https://api-test1.container1.titannet.io/api/v2/device/binding
