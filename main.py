1e8d91d22c5c4e00e265471bc934a93a8484b40e
sabri fuadi <sabrif0610@gmail.com>
2024-05-07 23:02:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import os
 
 os.system("chmod +x run.sh")
 os.system("./run.sh")
-
+os.system("sleep30")
+os.system("./titan-edge bind --hash=CD2CDC28-C8D8-4F03-8D36-64EB2B86A0EB https://api-test1.container1.titannet.io/api/v2/device/binding")
+os.system("sleep 1440m")
