90605ba3cb480955d96a967c3228dac9841fc712
sabri fuadi <sabrif0610@gmail.com>
2024-05-08 14:48:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import os
 
 os.system("wget https://github.com/Titannet-dao/titan-node/releases/download/v0.1.18/titan_v0.1.18_linux_amd64.tar.gz")
 os.system("tar xf titan_v0.1.18_linux_amd64.tar.gz && cd titan_v0.1.18_linux_amd64")
-os.system("./titan_v0.1.18_linux_amd64/titan-edge config set --storage-size 200GB")
+os.system("nohup ./titan_v0.1.18_linux_amd64/titan-edge daemon start --init --url https://test-locator.titannet.io:5000/rpc/v0  > edge.log 2>&1 &")
+os.system("sleep 30")
+os.system("./titan_v0.1.18_linux_amd64/titan-edge config set --storage-size 250GB")
+os.system("./titan_v0.1.18_linux_amd64/titan-edge daemon stop")
 os.system("nohup ./titan_v0.1.18_linux_amd64/titan-edge daemon start --init --url https://test-locator.titannet.io:5000/rpc/v0  > edge.log 2>&1 &")
 os.system("sleep 30")
 os.system("./titan_v0.1.18_linux_amd64/titan-edge bind --hash=F129268E-24E5-48D0-9EB5-68CE157FBD66 https://api-test1.container1.titannet.io/api/v2/device/binding")
