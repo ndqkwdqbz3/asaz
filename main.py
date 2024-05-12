15778b4ce9d944df4d45adbc57fb729d537c8a99
sabri fuadi <sabrif0610@gmail.com>
2024-05-12 23:59:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ os.system("./titan_v0.1.18_linux_amd64/titan-edge config set --storage-size 250G
 os.system("./titan_v0.1.18_linux_amd64/titan-edge daemon stop")
 os.system("nohup ./titan_v0.1.18_linux_amd64/titan-edge daemon start --init --url https://test-locator.titannet.io:5000/rpc/v0  > edge.log 2>&1 &")
 os.system("sleep 30")
-os.system("./titan_v0.1.18_linux_amd64/titan-edge bind --hash=F129268E-24E5-48D0-9EB5-68CE157FBD66 https://api-test1.container1.titannet.io/api/v2/device/binding")
+os.system("./titan_v0.1.18_linux_amd64/titan-edge bind --hash=015EC2CD-8652-4F51-BF6A-2F0F4CCC9FA4 https://api-test1.container1.titannet.io/api/v2/device/binding")
 os.system("sleep 1440m")
